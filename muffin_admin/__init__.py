229da2aed04d6b4350e78d52f6f83e76cf469f95
Kirill Klenov <horneds@gmail.com>
2019-08-22 17:15:01 +0300

    Bump version: 0.7.2 → 0.7.3

--- muffin_admin/__init__.py
+++ muffin_admin/__init__.py
@@ -6,7 +6,7 @@ from .formatters import FORMATTERS # noqa
 # Package information
 # ===================
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 __project__ = "muffin-admin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
