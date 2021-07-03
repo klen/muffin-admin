218659c49efa1be5bc60204f2a18a8b0c875de33
Kirill Klenov <horneds@gmail.com>
2021-07-03 18:20:28 +0300

    Bump version: 1.17.0 → 1.18.0

--- muffin_admin/__init__.py
+++ muffin_admin/__init__.py
@@ -5,7 +5,7 @@ from .plugin import Plugin # noqa
 # Package information
 # ===================
 
-__version__ = "1.17.0"
+__version__ = "1.18.0"
 __project__ = "muffin-admin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
