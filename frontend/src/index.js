7ede58c28aff2161540b7bae57a13531ff04bc5d
Kirill Klenov <horneds@gmail.com>
2022-05-01 23:08:01 +0300

    feat: wip

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,14 @@
+import { createRoot } from 'react-dom/client'
+
 import './admin'
+import { processAdmin } from './utils'
 
 export * from './utils'
+
+export async function initAdmin(prefix = '') {
+  const response = await fetch(`${prefix}/ra.json`)
+  const props = await response.json()
+  const container = document.getElementById('root')
+  const root = createRoot(container)
+  root.render(processAdmin('admin', props))
+}
--- frontend/src/web.js
+++ frontend/src/web.js
@@ -1,15 +1,3 @@
-import { createRoot } from 'react-dom/client'
+import { initAdmin } from '.'
 
-import { setupAdmin, processAdmin } from './utils'
-
-import './admin'
-
-const initAdmin = (globalThis.initAdmin = (props) => {
-  const container = document.getElementById('root')
-  const root = createRoot(container)
-  root.render(processAdmin('admin', props))
-})
-
-export { setupAdmin, processAdmin }
-
-export default initAdmin
+global.initAdmin = initAdmin
