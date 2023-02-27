4a037341642292ec06dbdbea928854caff575fcb
Kirill Klenov <horneds@gmail.com>
2023-02-27 13:37:40 +0700

    Bump version: 1.35.1 → 1.36.0

--- muffin_admin/__init__.py
+++ muffin_admin/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "1.35.1"
+__version__ = "1.36.0"
 __project__ = "muffin-admin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
