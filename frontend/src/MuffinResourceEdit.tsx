e3de9075a237740cefbae60f2deb4f062c0d02fd
Kirill Klenov <horneds@gmail.com>
2023-08-21 11:34:32 +0700

    fix: wip

--- frontend/src/MuffinResourceEdit.tsx
+++ frontend/src/MuffinResourceEdit.tsx
@@ -12,17 +12,20 @@ import { ActionButton } from "./buttons"
 import { AdminAction, AdminOpts, AdminResourceProps } from "./types"
 import { AdminPropsContext, buildAdmin, setupAdmin } from "./utils"
 
-export function MuffinResourceEdit({ actions, inputs, ...props }: AdminResourceProps["edit"]) {
+export function MuffinResourceEdit(props: AdminResourceProps["edit"]) {
   const resourceName = useResourceContext()
   const {
     adminProps: { mutationMode },
   } = useContext(AdminPropsContext) as AdminOpts
 
+  if (!props) return null
+  const { actions, inputs, ...opts } = props
+
   return (
     <Edit
       actions={buildAdmin(["edit-actions", resourceName], actions)}
       mutationMode={mutationMode || "optimistic"}
-      {...props}
+      {...opts}
     >
       <SimpleForm>{buildAdmin(["edit-inputs", resourceName], inputs)}</SimpleForm>
     </Edit>
