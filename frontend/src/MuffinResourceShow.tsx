fed6c035f101ea6a662925dab38e3520339e92e6
Kirill Klenov <horneds@gmail.com>
2024-06-07 16:03:51 +0300

    fix: wip

--- frontend/src/MuffinResourceShow.tsx
+++ frontend/src/MuffinResourceShow.tsx
@@ -10,12 +10,14 @@ import { LinkAction } from "./actions"
 import { buildRA } from "./buildRA"
 import { ActionButton } from "./buttons"
 import { AdminAction, AdminShowProps } from "./types"
-import { buildAdmin, setupAdmin } from "./utils"
+import { buildAdmin, findBuilder, setupAdmin } from "./utils"
 
 export function MuffinResourceShow(props: AdminShowProps) {
   const resourceName = useResourceContext()
+  const ShowActions = findBuilder(["show-actions", resourceName])
+
   return (
-    <Show actions={buildAdmin(["show-actions", resourceName], props)}>
+    <Show actions={<ShowActions {...props} />}>
       <SimpleShowLayout>{buildAdmin(["show-fields", resourceName], props)}</SimpleShowLayout>
     </Show>
   )
@@ -23,19 +25,22 @@ export function MuffinResourceShow(props: AdminShowProps) {
 
 setupAdmin(["show"], (props) => <MuffinResourceShow {...props} />)
 setupAdmin(["show-fields"], ({ fields }) => buildRA(fields))
-setupAdmin(["record-actions"], (actions: AdminAction[]) =>
+setupAdmin(["record-actions"], ({ actions }: { actions: AdminAction[] }) =>
   actions.map((props, idx) => <ActionButton key={idx} {...props} />)
+)
+setupAdmin(["record-links"], ({ links }: { links: AdminShowProps["links"] }) =>
+  links.map(([key, props]) => <LinkAction key={key} resource={key} {...props} />)
 )
 setupAdmin(["show-actions"], ({ actions, links, edit }: AdminShowProps) => {
   const resourceName = useResourceContext()
+  const Actions = findBuilder(["record-actions", resourceName])
+  const Links = findBuilder(["record-links", resourceName])
   return (
     <TopToolbar>
       <div style={{ marginRight: "auto" }}>
-        {links.map(([key, props]) => (
-          <LinkAction key={key} resource={key} {...props} />
-        ))}
+        <Links links={links} />
       </div>
-      {buildAdmin(["record-actions", resourceName], actions)}
+      <Actions actions={actions} />
       <ListButton />
       {edit && <EditButton />}
     </TopToolbar>
