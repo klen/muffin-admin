5aa6509b56d69c172e12f42336220f9bbe5d7665
Kirill Klenov <horneds@gmail.com>
2024-06-14 11:44:20 +0300

    fix: json field

--- frontend/src/fields/JsonField.tsx
+++ frontend/src/fields/JsonField.tsx
@@ -5,7 +5,10 @@ import { Button, FunctionField, useRecordContext } from "react-admin"
 export function JsonField({ label, source }) {
   const [expand, setExpand] = useState(false)
   const record = useRecordContext()
-  const src = JSON.stringify(get(record, source), null, 2)
+  const value = get(record, source)
+  if (!value || Object.keys(value).length == 0) return null
+
+  const src = JSON.stringify(value, null, 2)
   const retval = (
     <div>
       <p></p>
@@ -15,7 +18,10 @@ export function JsonField({ label, source }) {
         color="primary"
         size="small"
         label={expand ? "colapse" : "expand"}
-        onClick={() => setExpand(!expand)}
+        onClick={(e) => {
+          setExpand(!expand)
+          e.stopPropagation()
+        }}
       />
       <p>{expand && <pre>{src}</pre>}</p>
     </div>
